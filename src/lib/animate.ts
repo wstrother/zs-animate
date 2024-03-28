90830b82539eae247344fb7d7deb20ba39d9e730
Wyatt Strother <wstrother@users.noreply.github.com>
2024-03-28 01:22:37 -0400

    serve static images and gitignore them

--- src/lib/animate.ts
+++ src/lib/animate.ts
@@ -1,14 +1,18 @@
 import { Application, Assets } from "pixi.js";
 
 // load image textures
-// async function loadAssets(assets: Array<String>) {
-
-// }
+async function loadAssets(assets: Array<String>) {
+    // const textures = [];
+    assets.forEach(async name => {
+        await Assets.load(`images/${name}`);
+    })
+}
 
 export default {
     createApp: async (canvasElement: HTMLElement) => {
         const app = new Application();
-        console.log(app);
+        loadAssets(['face.png']);
+
         await app.init({ 
             resizeTo: canvasElement,
             backgroundColor: '#FF0000'
