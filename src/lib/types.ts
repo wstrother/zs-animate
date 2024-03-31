ee4fb0bb127128ad983a72daadcce0eadb729f87
Wyatt Strother <wstrother@users.noreply.github.com>
2024-03-31 03:08:50 -0400

    sprites members to manifest type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,5 +3,6 @@ import type { SpritesheetData } from "pixi.js"
 export type Manifest = {
     textures: Record<string, string>,
     sheets: Record<string, SpritesheetData>,
-    json: Record<string, string>
+    json: Record<string, string>,
+    sprites: Record<string, {}>,
 }
