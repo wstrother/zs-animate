ae5e06874d1adc9fabc8b7ed73089a9b92bf1856
Wyatt Strother <wstrother@users.noreply.github.com>
2024-03-31 06:26:56 -0400

    points in sprite data can be passed as array

--- src/lib/sprites.ts
+++ src/lib/sprites.ts
@@ -1,41 +1,29 @@
-import { ObservablePoint, Sprite } from "pixi.js";
-import type { SpriteData } from "./types";
+import { Sprite, type SpriteOptions } from "pixi.js";
+import type { AppContext, SpriteData } from "./types";
 
-function setSpriteData(sprite: Sprite, data: SpriteData) { 
-    // need to exclude any expected keys from sprite data
-    // that do not refer to editable members of the Pixi sprite class
-    const excludeKeys: Array<string> = ['texture', 'spritesheet'];
 
-    for (const key in data) {
-        if (sprite[key as keyof Sprite] instanceof ObservablePoint) {
-            // infers keys that refer to points with x, y values 
-            const [x, y] = data[key as keyof SpriteData] as [number, number];
-            const point = sprite[key as keyof Sprite] as ObservablePoint;
-            point.x = x;
-            point.y = y;
-
-        } else if (!excludeKeys.includes(key)) {
-            // catch all asignment, to pass whatever sprite data can be
-            // assigned directly from provided JSON
-            // @ts-ignore       <-- suppress error for possible readonly members
-            sprite[key as keyof Sprite] = data[key as keyof SpriteData]
-        }
+export function createSprite(data: SpriteData, {textures, spritesheets}: AppContext): Sprite {
+    // replace texture and spritesheet aliases
+    if (data.texture && typeof data.texture === 'string') data.texture = textures[data.texture];
+    if (data.spritesheet && typeof data.spritesheet === 'string') {
+        data.spritesheet = spritesheets[data.spritesheet];
+        // TODO: assign texture field from spritesheet
     }
-}
 
-export function createSprite(data: SpriteData): Sprite {
-    const sprite = (() => {
-        // create static sprite from single Texture
-        if (data.texture) {
-            return Sprite.from(data.texture)
-        }
-        // create dynamic sprite from Spritesheet
-        //  TODO
-    })();
+    // ensure all point data is consitent with Pixi's {x, y} structure
+    const isPoint = (point: any): boolean => {
+        if (!(point instanceof Array)) return false;
+        if (!(point.length === 2)) return false;
+        if (!(typeof point[0] === 'number' && typeof point[1] === 'number')) return false;
+        return true;
+    }
+    for (const key of Object.keys(data)
+        .filter(k => isPoint(data[k as keyof SpriteData]))
+    ) {
+        const point = data[key as keyof SpriteData]
+        // @ts-ignore
+        data[key] = {x: point[0], y: point[1]}
+    }
 
-    if (!sprite) throw Error('SpriteData provided could not be used');
-
-    setSpriteData(sprite, data);
-
-    return sprite;
+    return new Sprite({...data as SpriteOptions})
 }
