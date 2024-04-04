4e420f765fdcd994c542fb292bd8395edf617de0
Wyatt Strother <wstrother@users.noreply.github.com>
2024-04-03 21:03:33 -0400

    load manifest with ajax

--- src/routes/game/[[manifest]]/+page.svelte
+++ src/routes/game/[[manifest]]/+page.svelte
@@ -1,40 +1,53 @@
 <script lang='ts'>
     import { loadedJSON, loadedSpritesheets, loadedTextures, appStage } from '$lib/stores.js';
-
-    export let data;
-    let destroyApp: undefined | Function
+    import create from '$lib/create';
     
-    const create = async () => {
+    export let data;
+    let destroyApp: undefined | Function;
+    let manifestError: boolean = false;
+    let manifestErrMessage: string = '';
+    
+    
+    const loadApp = async () => {
         if (destroyApp) destroyApp();
 
-        if (!data.manifestError) {
-            const canvasElement = document.getElementById('appCanvas');
-            if (canvasElement) {
-                await data.animate.createApp(canvasElement, data.manifest)
-                    .then(({ textures, spritesheets, jsonFiles, stage, destroy }) => {
-                        loadedTextures.set(textures);
-                        loadedJSON.set(jsonFiles);
-                        loadedSpritesheets.set(spritesheets);
-                        appStage.set(stage);
-                        destroyApp = destroy;
-                    });
-            }
+        // if (!data.manifestError) {
+        //     const canvasElement = document.getElementById('appCanvas');
+        //     if (canvasElement) {
+        //         await data.animate.createApp(canvasElement, data.manifest)
+        //             .then(({ textures, spritesheets, jsonFiles, stage, destroy }) => {
+        //                 loadedTextures.set(textures);
+        //                 loadedJSON.set(jsonFiles);
+        //                 loadedSpritesheets.set(spritesheets);
+        //                 appStage.set(stage);
+        //                 destroyApp = destroy;
+        //             });
+        //     }
+        // }
+
+        const resp = await fetch(`/api/json/${data.manifestFile}`);
+        if (resp.ok) {
+            manifestError = false;
+            const jsonData = await resp.json();
+            console.log(jsonData);
+        } else {
+            manifestError = true;
+            manifestErrMessage = resp.statusText;
         }
     }
 </script>
 
 <div id="appCanvas">
-    {#if data.manifestError }
+    {#if manifestError }
         <div class="ml-5">
             <h3 class="h3">Error Loading Manifest file</h3>
-            <p>Could not load file at path:</p> 
-                <pre>json/{data.manifestFile}.json</pre>
+                <pre>{manifestErrMessage}</pre>
         </div>
     {/if}
 </div>
 
 <div id="controls">
-    <button on:click={create} class="btn btn-md">Create app</button>
+    <button on:click={loadApp} class="btn btn-md">Create app</button>
 </div>
 
 <style>
--- src/routes/game/[[manifest]]/+page.ts
+++ src/routes/game/[[manifest]]/+page.ts
@@ -1,51 +1,10 @@
 import type { PageLoad } from './$types';
-import create from '$lib/create';
-
-
-function findJson(data: any, filenames: Set<string>): void {
-    if (typeof data === 'string') {
-        if (data.endsWith('.json')) {
-            filenames.add(data);
-        }
-    } else if (Array.isArray(data)) {
-        for (const item of data) {
-            findJson(item, filenames);
-        }
-    } else if (typeof data === 'object' && data !== null) {
-        for (const key in data) {
-            findJson(data[key], filenames);
-        }
-    }
-}
 
 
 export const load = (async ({ fetch, params }) => {
     const manifestFile = params?.manifest?.replace('/','') || 'start';
-    let manifestError = false;
-
-    const innerJson = new Set<string>();
-
-    const manifest = await fetch(`/json/${manifestFile}.json`)
-        .then(resp => {
-            if (resp.status !== 200) { manifestError = true; }
-            return resp.json(); 
-        })
-        .then(json => {
-            // search for inner JSON files only goes one level from manifest file
-            // so as to not need loop detection
-            findJson(json, innerJson);
-            return json;
-        })
-        .catch(err => {
-            console.log(err);
-            manifestError = true;
-        });
 
     return {
-        animate: create,
-        manifest,
-        innerJson,
         manifestFile,
-        manifestError
     };
 }) satisfies PageLoad;
