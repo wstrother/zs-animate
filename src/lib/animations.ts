c0e9690b3b4716d49858530e26c5ae6a57a02803
Wyatt Strother <wstrother@users.noreply.github.com>
2024-04-13 17:53:11 -0400

    create animations.ts

---
+++ src/lib/animations.ts
@@ -0,0 +1,7 @@
+
+
+// This module creates an interface for parsing animation data 
+// in a flexible variety of different formats/specifications
+// and provides a helper function that can translate them to fit
+// the specifications of the PIXI.js spritesheet API
+
